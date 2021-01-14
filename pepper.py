508ea5e6b81b809f23c6e609109c4e5335f917c8
Chr0nos <snicolet@student.42.fr>
2021-01-14 15:35:38 +0100

    fixes

--- pepper.py
+++ pepper.py
@@ -11,6 +11,7 @@ def setup_pepper(path='/mnt', boot_dev='/dev/vda'):
     run(['vim', '/etc/pacman.conf'])
     run(['vim', '/etc/locale.gen'])
     run('echo pepper > /etc/hostname', shell=True)
+    os.makedirs(f'{path}/etc/', exist_ok=True)
     run(['cp', '-v', '/etc/resolv.conf', f'{path}/etc/resolv.conf'])
     Pacman.sync()
     with arch_chroot(path):
@@ -18,11 +19,12 @@ def setup_pepper(path='/mnt', boot_dev='/dev/vda'):
             'net-tools', 'networkmanager', 'wireguard-tools', 'fail2ban',
             'iptables', 'grub', 'vim', 'zsh', 'git', 'gcc', 'clang', 'docker',
             'linux', 'linux-headers', 'linux-firmware', 'mkinitramfs', 'mdadm',
-            'archlinux-keyring', 'sudo', 'wget', 'xfsprogs'
+            'archlinux-keyring', 'sudo', 'wget', 'xfsprogs', 'tmux'
         ])
         run(['grub-mkconfig', '-o', '/boot/grub.cfg'])
         run(['mkinitcpio', '-p', 'linux'])
-        for service in ('iptables', 'fail2ban', 'docker', 'networkmanager'):
+        services = ('iptables', 'fail2ban', 'docker', 'networkmanager', 'ssh')
+        for service in services:
             run(['systemctl', 'enable', service])
         os.makedirs('/etc/polkit-1/rules.d/', exist_ok=True)
         run(['ln', '-s', '/usr/share/zoneinfo/Europe/Paris', '/etc/localtime'])
